116d541e0e87ec596f776b4c17dcfcaa93420681
Snehil Banerjee <snehil.banerjee@yappes.com>
2018-08-09 17:03:28 +0530

    added patch and delete methods

--- ypconnector.php
+++ ypconnector.php
@@ -118,5 +118,89 @@ class yappesLibrary
       echo $e;
       }
     }
+
+
+    function deleteOperation($url, $parameters)
+    {
+    $options = ["host" => parse_url($url, PHP_URL_HOST) , "path" => parse_url($url, PHP_URL_PATH) , "port" => parse_url($url, PHP_URL_PORT) , "method" => "get", "headers" => $parameters["headers"]];
+    if (!$options["port"])
+      {
+      $options["port"] = 443;
+      }
+    try
+      {
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_HEADER, true);
+      curl_setopt($ch, CURLOPT_NOBODY, false);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $parameters["headers"]);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+      curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+      curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+      $output = curl_exec($ch);
+      $info = curl_getinfo($ch);
+      $header_len = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+      $header = substr($output, 0, $header_len);
+      $body = substr($output, $header_len);
+      curl_close($ch);
+      $responseSchema["headers"] = $header;
+      $responseSchema["statusCode"] = $info["http_code"];
+      $responseSchema["statusMessage"] = "";
+      $responseSchema["body"] = $body;
+      return $responseSchema;
+      }
+    catch(Exception $e)
+      {
+      echo $e;
+      }
+    }
+  
+
+    function patchOperation($url, $parameters)
+    {
+    $options = ["host" => parse_url($url, PHP_URL_HOST) , "path" => parse_url($url, PHP_URL_PATH) , "port" => parse_url($url, PHP_URL_PORT) , "method" => "get", "headers" => $parameters["headers"]];
+    if (!$options["port"])
+      {
+      $options["port"] = 443;
+      }
+    try
+      {
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PATCH");
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $parameters["payload"]);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $parameters["headers"]);
+      curl_setopt($ch, CURLOPT_NOBODY, false);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_VERBOSE, 1);
+      curl_setopt($ch, CURLOPT_HEADER, 1);
+      curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+      curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+      $output = curl_exec($ch);
+      $info = curl_getinfo($ch);
+      $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+      $header = substr($output, 0, $header_size);
+      $body = substr($output, $header_size);
+      curl_close($ch);
+      $responseSchema["headers"] = $header;
+      $responseSchema["statusCode"] = $info["http_code"];
+      $responseSchema["statusMessage"] = "";
+      $responseSchema["body"] = $body;
+      return $responseSchema;
+      }
+    catch(Exception $e)
+      {
+      echo $e;
+      }
+    }
+
+
+
   }
 ?>
